4961737070bcf1a3fd99510ac4e950f27f31bd12
marisalvarani <mari_salvarani@hotmail.com>
2021-04-28 09:08:15 -0300

    Implementação das rotas

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+  {path:'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
